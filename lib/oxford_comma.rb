4171c68235d15eb17e67fc40ea971b37c5631410
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-17 15:14:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,9 @@ def oxford_comma(array)
   if (array.size == 1)
     return array[0]
   end
-  
+  if (array.size == 2)
+    return array.join(" and ")
+  end
   last = ", and #{array.pop}"
   return (array.join(", ")) << last
 end
